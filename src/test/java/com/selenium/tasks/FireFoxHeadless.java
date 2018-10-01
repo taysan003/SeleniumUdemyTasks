65397ebd12bf08220f7bc6114921acb4a07726b8
Andrei <taysan003@gmail.com>
2018-10-01 17:25:53 -0400

    FirefoxHeadless

--- src/test/java/com/selenium/tasks/FireFoxHeadless.java
+++ src/test/java/com/selenium/tasks/FireFoxHeadless.java
@@ -1,4 +1,21 @@
 package com.selenium.tasks;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
 public class FireFoxHeadless {
+    public static void main(String[] args) {
+        FirefoxBinary firefoxBinary = new FirefoxBinary();
+        firefoxBinary.addCommandLineOptions("--headless");
+
+        FirefoxOptions fo = new FirefoxOptions();
+        fo.setBinary(firefoxBinary);
+        WebDriver driver = new FirefoxDriver(fo);
+        driver.get("http://facebook.com");
+        String title = driver.getTitle();
+        System.out.println(title);
+
+    }
 }
